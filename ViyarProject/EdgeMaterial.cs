5111c0af10e3f31d7d6b9ed6f86336ce757e399c
viachpaliy <viachpaliy@ukr.net>
2021-05-28 22:18:48 +0300

    add class for edge material

---
+++ ViyarProject/EdgeMaterial.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Xml.Serialization;
+using System.Globalization;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace ViyarProject
+{
+     
+    [Serializable] 
+    public class EdgeMaterial : AbstractMaterial
+    {       
+        [XmlAttribute]
+        public string article = "42691";
+        [XmlAttribute]
+        public string guid = "42691";
+        [XmlAttribute]
+        public string oldGuid = "42691";
+        [XmlAttribute]
+        public string uuid = "835c60ac-c2ca-11e7-b5a5-005056b13af7";
+        [XmlAttribute]
+        public string name ="������ ��������� 22�0,5��";
+        [XmlAttribute]
+        public string height ="22";
+        [XmlAttribute]
+        public string thickness = "0.5";
+        [XmlAttribute]
+        public string markingColor="rgb(255,0,0)";
+
+        public EdgeMaterial()
+        {
+            id = "2";
+            type = "band";
+        }
+    }
+}
